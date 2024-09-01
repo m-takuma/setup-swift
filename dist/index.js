46560f9147d6dc05091a9fff2372632bc1359337
m-takuma <m.takuma.dev@gmail.com>
2024-09-01 21:53:51 +0900

    fix: import to require

--- dist/index.js
+++ dist/index.js
@@ -28850,13 +28850,21 @@ async function install_swift_on_mac(pkg_path) {
 /***/ }),
 
 /***/ 1980:
-/***/ (() => {
+/***/ ((module) => {
 
- const IS_WINDOWS = process.platform === "win32";
- const IS_MAC = process.platform === "darwin";
- const IS_LINUX = process.platform === "linux";
- const IS_AARCH64 = process.arch === "arm64";
- const IS_X64 = process.arch === "x64";
+const IS_WINDOWS = process.platform === "win32";
+const IS_MAC = process.platform === "darwin";
+const IS_LINUX = process.platform === "linux";
+const IS_AARCH64 = process.arch === "arm64";
+const IS_X64 = process.arch === "x64";
+
+module.exports = {
+    IS_WINDOWS,
+    IS_MAC,
+    IS_LINUX,
+    IS_AARCH64,
+    IS_X64
+};
 
 /***/ }),
 
