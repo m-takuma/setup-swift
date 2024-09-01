6aafdcbca5cbcad3f475736059c95b73928e958f
m-takuma <m.takuma.dev@gmail.com>
2024-09-01 21:53:39 +0900

    fix: import to require

--- src/utils/os.js
+++ src/utils/os.js
@@ -1,5 +1,13 @@
- const IS_WINDOWS = process.platform === "win32";
- const IS_MAC = process.platform === "darwin";
- const IS_LINUX = process.platform === "linux";
- const IS_AARCH64 = process.arch === "arm64";
- const IS_X64 = process.arch === "x64";
+const IS_WINDOWS = process.platform === "win32";
+const IS_MAC = process.platform === "darwin";
+const IS_LINUX = process.platform === "linux";
+const IS_AARCH64 = process.arch === "arm64";
+const IS_X64 = process.arch === "x64";
+
+module.exports = {
+    IS_WINDOWS,
+    IS_MAC,
+    IS_LINUX,
+    IS_AARCH64,
+    IS_X64
+};
