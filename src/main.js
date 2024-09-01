0927803c78486a1079cf1f41dc6378999bbb049d
m-takuma <m.takuma.dev@gmail.com>
2024-09-01 22:13:05 +0900

    fix: import to require

--- src/main.js
+++ src/main.js
@@ -18,11 +18,11 @@ async function run () {
   if (IS_MAC) {
     console.log('Setting up Swift on macOS');
     core.debug('Setting up Swift on macOS');
-    setup_swift_on_mac('5.10.1');
+    await setup_swift_on_mac('5.10.1');
   } else if (IS_LINUX) {
     console.log('Setting up Swift on Linux');
     core.debug('Setting up Swift on Linux');
-    setup_swift_on_linux('5.10.1');
+    await setup_swift_on_linux('5.10.1');
   } else if (IS_WINDOWS) {
     console.log('Setting up Swift on Windows');
     core.debug('Setting up Swift on Windows');
